b902119a5a77687e0dd65d9eadd6057072462d5b
Michał Kurek <mk-02@wp.pl>
2023-03-30 21:13:27 -0400

    First commit!

---
+++ Piece.cpp
@@ -0,0 +1,43 @@
+//#include "Piece.h"
+class Piece {
+    public:
+    static const int None = 0;
+    static const int King = 1;
+    static const int Pawn = 2;
+    static const int Knight = 3;
+    static const int Bishop = 5;
+    static const int Rook = 6;
+    static const int Queen = 7;
+
+    static const int White = 8;
+    static const int Black = 16;
+
+    static const int typeMask = 0b00111;
+    static const int blackMask = 0b10000;
+    static const int whiteMask = 0b01000;
+    static const int colorMask = whiteMask | blackMask;
+
+    static bool IsColour (int piece, int colour) {
+        return (piece & colorMask) == colour;
+    }
+
+    static int Colour (int piece) {
+        return piece & colorMask;
+    }
+
+    static int PieceType (int piece) {
+        return piece & typeMask;
+    }
+
+    static bool IsRookOrQueen (int piece) {
+        return (piece & 0b110) == 0b110;
+    }
+
+    static bool IsBishopOrQueen (int piece) {
+        return (piece & 0b101) == 0b101;
+    }
+
+    static bool IsSlidingPiece (int piece) {
+        return (piece & 0b100) != 0;
+    }
+};
